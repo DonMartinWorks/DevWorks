c57881a5f86489d6d8ff1c72cdc7c02b21801aeb
DonMartinWorks <donmartinworks@gmail.com>
2023-03-21 14:23:23 -0300

    Validacion de los campos y previsualizacion de la imagen (create-vacancy.blade)

--- app/Http/Livewire/Forms/CreateVacancy.php
+++ app/Http/Livewire/Forms/CreateVacancy.php
@@ -5,19 +5,22 @@ namespace App\Http\Livewire\Forms;
 use App\Models\Category;
 use App\Models\Salary;
 use Livewire\Component;
+use Livewire\WithFileUploads;
 
 class CreateVacancy extends Component
 {
     public $title, $salary, $category, $company, $last_day, $description, $image;
 
+    use WithFileUploads;
+
     protected $rules = [
         'title' => 'required|string|max:60',
         'salary' => 'required|numeric|between:1,9',
         'category' => 'required|numeric|between:1,7',
-        'company' => 'required|string',
+        'company' => 'required|string|max:60',
         'last_day' => 'required|date',
-        'description' => 'required|string',
-        'image' => 'required'
+        'description' => 'required|string|max:450',
+        'image' => 'required|image|max:1024'
     ];
 
     # ** Funcion para crear una nueva vacante **
---
+++ resources/views/livewire/component/show-alert.blade.php
@@ -0,0 +1,3 @@
+<div class="w-full py-1 text-md bg-rose-500 text-white text-center rounded-md mt-5">
+    <p>{{ $message }}</p>
+</div>
